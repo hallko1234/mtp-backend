5e100a79baf071e8568e396e38f6d5a4bf4faf7e
hallko1234 <149827893+hallko1234@users.noreply.github.com>
2025-02-11 20:26:01 +0100

    Create Models.py

---
+++ Models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+    role = Column(String, default="standard")
+
+class File(Base):
+    __tablename__ = "files"
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String)
+    folder = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+class ShareLink(Base):
+    __tablename__ = "share_links"
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String)
+    expiration = Column(Integer)
