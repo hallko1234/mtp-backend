28a47589b8f726c5d24af257f74a465843a62df2
hallko1234 <149827893+hallko1234@users.noreply.github.com>
2025-02-11 20:25:10 +0100

    Create Database.py

---
+++ Database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./mtp.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def create_db():
+    Base.metadata.create_all(bind=engine)
