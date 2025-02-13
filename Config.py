0f9ef502ec26b1200126a30bda16f8d0ba4d4311
hallko1234 <149827893+hallko1234@users.noreply.github.com>
2025-02-13 23:57:33 +0100

    Config.py

--- Config.py
+++ Config.py
@@ -1,3 +1,8 @@
+# Geheimer Schlüssel für JWT-Authentifizierung
 SECRET_KEY = "super-sicherer-schluessel"
+
+# Pfad für gespeicherte Dateien
 UPLOAD_DIR = "static/"
+
+# Maximale Videogröße in MB
 MAX_VIDEO_SIZE_MB = 100
